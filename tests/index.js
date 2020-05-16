ad52e43f05fb14b1770a0bc4a859fe1c55010865
Gildas <gildas-lormeau@users.noreply.github.com>
2020-05-16 23:15:32 +0200

    refactoring

--- tests/index.js
+++ tests/index.js
@@ -4,74 +4,76 @@
     const VIDEO_FPS = 15;
     const VIDEO_TIME_OFFSET = 0.0125;
 
-    const viewer = document.getElementById("viewer");
-    const progress = document.getElementById("progress");
-    const form = document.forms[0];
+    const viewerElement = document.getElementById("viewer");
+    const progressElement = document.getElementById("progress");
+    const formElement = document.forms[0];
     let images, width, height, maxFrames, fps, workers, animationFrameId;
 
-    form.onsubmit = async event => {
+    formElement.onsubmit = async event => {
         images = [];
-        width = Number(form.width.value);
-        height = Number(form.height.value);
-        maxFrames = Number(form.count.value) - 1;
-        workers = Number(form.workers.value);
+        width = Number(formElement.width.value);
+        height = Number(formElement.height.value);
+        maxFrames = Number(formElement.count.value) - 1;
+        workers = Number(formElement.workers.value);
         fps = VIDEO_FPS / (360 / (maxFrames + 1));
         event.preventDefault();
         if (animationFrameId) {
             cancelAnimationFrame(animationFrameId);
         }
-        progress.hidden = false;
-        viewer.hidden = true;
-        progress.value = 0;
-        progress.max = maxFrames;
+        progressElement.hidden = false;
+        viewerElement.hidden = true;
+        progressElement.value = 0;
+        progressElement.max = maxFrames;
         await decodeFrames();
-        progress.hidden = true;
-        viewer.hidden = false;
+        progressElement.hidden = true;
+        viewerElement.hidden = false;
         viewImages();
     };
 
     function decodeFrames() {
-        const promises = [];
+        const workerPromises = [];
         for (let index = 0; index < workers; index++) {
-            promises.push(workerDecodeFrames(index / fps));
+            workerPromises.push(workerDecodeFrames(index / fps));
         }
-        return Promise.all(promises);
+        return Promise.all(workerPromises);
     }
 
     function workerDecodeFrames(startTime) {
-        return new Promise(resolve => {
-            const loader = document.createElement("video");
-            const decoder = document.createElement("canvas");
-            const decoderContext = decoder.getContext("2d");
-            loader.addEventListener("canplay", loadImages);
-            decoder.width = viewer.width = width;
-            decoder.height = viewer.height = height;
-            loader.src = VIDEO_FILENAME;
-
-            function loadImages() {
-                loader.removeEventListener("canplay", loadImages);
-                loader.addEventListener("seeked", captureFrame);
-                loader.currentTime = startTime + VIDEO_TIME_OFFSET;
-            }
-
-            function captureFrame() {
-                decoderContext.drawImage(loader, 0, 0, width, height);
-                const currentFrame = Math.floor(loader.currentTime * fps);
-                images[currentFrame] = decoderContext.getImageData(0, 0, width, height);
-                progress.value++;
-                if (currentFrame < maxFrames) {
-                    loader.currentTime = Math.min(((currentFrame + workers) / fps) + VIDEO_TIME_OFFSET, loader.duration);
-                } else {
-                    loader.removeEventListener("seeked", captureFrame);
-                    resolve();
-                }
-            }
+        return new Promise((resolve, reject) => {
+            const videoElement = document.createElement("video");
+            const canvasElement = document.createElement("canvas");
+            canvasElement.width = viewerElement.width = width;
+            canvasElement.height = viewerElement.height = height;
+            videoElement.src = VIDEO_FILENAME;
+            videoElement.onerror = reject;
+            videoElement.oncanplay = () => oncanplay(videoElement, startTime);
+            videoElement.onseeked = () => onseeked(videoElement, canvasElement.getContext("2d"), () => {
+                videoElement.onseeked = null;
+                resolve();
+            });
         });
+    }
+
+    function oncanplay(videoElement, startTime) {
+        videoElement.oncanplay = null;
+        videoElement.currentTime = startTime + VIDEO_TIME_OFFSET;
+    };
+
+    function onseeked(videoElement, decoderContext, callback) {
+        decoderContext.drawImage(videoElement, 0, 0, width, height);
+        const currentFrame = Math.floor(videoElement.currentTime * fps);
+        images[currentFrame] = decoderContext.getImageData(0, 0, width, height);
+        progressElement.value++;
+        if (currentFrame < maxFrames) {
+            videoElement.currentTime = Math.min(((currentFrame + workers) / fps) + VIDEO_TIME_OFFSET, videoElement.duration);
+        } else {
+            callback();
+        }
     }
 
     function viewImages(frameNumber = 0) {
         animationFrameId = requestAnimationFrame(() => {
-            viewer.getContext("2d").putImageData(images[frameNumber], 0, 0);
+            viewerElement.getContext("2d").putImageData(images[frameNumber], 0, 0);
             viewImages((frameNumber + 1) % maxFrames);
         });
     }
